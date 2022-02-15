30b1757ebc98e26e9811d26adebf4020717e2445
kkoon9 <rndrnjs2003@gmail.com>
2022-02-15 13:49:16 +0900

    fix: 코드 리뷰 반영 [1] - 상수는 private static final로 선언
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/racingcar/CarDistance.java
+++ src/main/java/racingcar/CarDistance.java
@@ -3,11 +3,11 @@ package racingcar;
 import static racingcar.TextUtil.printDistanceByHyphen;
 
 public class CarDistance {
-    private final int CAN_MOVE_VALUE = 4;
-    private final int MAX_MOVE_VALUE = 9;
-    private final int ZERO = 0;
-    private final int MOVE_VALUE = 1;
-    private final String MOVE_EXCEPTION_MESSAGE = "파라미터가 음수 혹은 9 초과이면 안됩니다.";
+    private static final int CAN_MOVE_VALUE = 4;
+    private static final int MAX_MOVE_VALUE = 9;
+    private static final int ZERO = 0;
+    private static final int MOVE_VALUE = 1;
+    private static final String MOVE_EXCEPTION_MESSAGE = "파라미터가 음수 혹은 9 초과이면 안됩니다.";
     private int distance;
 
     public CarDistance(int distance) {
--- src/main/java/racingcar/RacingDistanceCount.java
+++ src/main/java/racingcar/RacingDistanceCount.java
@@ -1,8 +1,8 @@
 package racingcar;
 
 public class RacingDistanceCount {
-    private final String DISTANCE_COUNT_EXCEPTION_MESSAGE = "이동횟수가 null이면 안 됩니다.";
-    private final String DISTANCE_COUNT_NEGATIVE_EXCEPTION_MESSAGE = "이동횟수가 음수이면 안 됩니다.";
+    private static final String DISTANCE_COUNT_EXCEPTION_MESSAGE = "이동횟수가 null이면 안 됩니다.";
+    private static final String DISTANCE_COUNT_NEGATIVE_EXCEPTION_MESSAGE = "이동횟수가 음수이면 안 됩니다.";
     private Integer distanceCount;
 
     public RacingDistanceCount(Integer distanceCount) {
