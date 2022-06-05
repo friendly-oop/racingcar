3c7f4bf336030ce67fe27764c7c2933b5d8d27db
kkoon9 <rndrnjs2003@gmail.com>
2022-06-05 11:59:40 +0900

    feat: 난수 생성기 - Red
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/test/java/racingcar/util/RandomNumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package racingcar.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumberGeneratorTest {
+    private RandomNumberGenerator randomNumberGenerator = new RandomNumberGeneratorTest();
+    private List<Integer> expectedNumbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    @DisplayName("0 ~ 9 사이에 난수를 생성한다.")
+    @Test
+    void print_car_distance_by_hyphen() {
+        // when
+        int randomNumber = randomNumberGenerator.pullNumber();
+
+        // then
+        assertThat(randomNumber).isIn(expectedNumbers);
+    }
+
+}
