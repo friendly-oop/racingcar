913b2bac330e9792ed6bb376942cbdf2705d312b
kkoon9 <rndrnjs2003@gmail.com>
2022-06-06 19:32:07 +0900

    feat: 경기 수가 0 이하이면 예외를 던진다 - GREEN
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -4,12 +4,14 @@ public class Race {
     private static final int MIN_PARTICIPANT_COUNT = 1;
     private static final int MIN_GAME_COUNT = 1;
     private static final String VALID_PARTICIPANT_COUNT_MESSAGE = "참가 자동차 수는 1 이상이어야 합니다.";
+    private static final String VALID_GAME_COUNT_MESSAGE = "경기 수는 1 이상이어야 합니다.";
 
     private Participant participantCount;
     private Game gameCount;
 
     public static Race of(final int participantCount, final int gameCount) {
         validateParticipantCount(participantCount);
+        validateGameCount(gameCount);
 
         Race race = new Race();
         race.participantCount = Participant.of(participantCount);
@@ -21,6 +23,12 @@ public class Race {
     private static void validateParticipantCount(final int participantCount) {
         if(participantCount < MIN_PARTICIPANT_COUNT) {
             throw new IllegalArgumentException(VALID_PARTICIPANT_COUNT_MESSAGE);
+        }
+    }
+
+    private static void validateGameCount(final int gameCount) {
+        if(gameCount < MIN_GAME_COUNT) {
+            throw new IllegalArgumentException(VALID_GAME_COUNT_MESSAGE);
         }
     }
 }
