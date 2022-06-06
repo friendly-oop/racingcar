86df8936e7914a99ee4323db63c073aa335bb6e7
kkoon9 <rndrnjs2003@gmail.com>
2022-06-06 19:14:48 +0900

    feat: 참가 자동차 수가 0 이하이면 예외를 던진다. - RED
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,5 +1,23 @@
 package racingcar;
 
-public class RaceTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class RaceTest {
+    private static final String VALID_PARTICIPANT_MESSAGE = "참가 자동차 수는 1 이상이어야 합니다.";
+
+    @DisplayName("참가 자동차 수가 0 이하이면 예외를 던진다.")
+    @Test
+    void convert_object_to_xml_string_non_static_error() {
+        // given
+        int givenParticipantNumber = 0;
+        int givenGameNumber = 1;
+        // then
+        assertThatThrownBy(() ->
+                Race.of(givenParticipantNumber, givenGameNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(VALID_PARTICIPANT_MESSAGE);
+    }
 }
